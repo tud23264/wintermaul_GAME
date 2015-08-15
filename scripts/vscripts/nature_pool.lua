5ef25f8f30b9ba3975486b023ece192251c88106
Eks2 <einceleme@hotmail.com>
2015-08-15 13:44:59 +0100

    First commit that makes gold from towers working.
    Signed-off-by: Eks2 <einceleme@hotmail.com>

--- scripts/vscripts/nature_pool.lua
+++ scripts/vscripts/nature_pool.lua
@@ -3,10 +3,13 @@ require('buildinghelper')
 BUILD_TIME=1.0
 
 function getBuildingPoint(keys)
-	local point = BuildingHelper:AddBuildingToGrid(keys.target_points[1], 2, keys.caster)
+	--local point = BuildingHelper:AddBuildingToGrid(keys.target_points[1], 2, keys.caster)
+	local ability = keys.ability
+	point = ability:GetCursorPosition()
 	if point ~= -1 then
-		local tower = CreateUnitByName("nature_pool", point, false, nil, nil, DOTA_TEAM_GOODGUYS)
+		local tower = CreateUnitByName("nature_pool", point, false, keys.caster, keys.caster, DOTA_TEAM_GOODGUYS)
 		BuildingHelper:AddBuilding(tower)
+		tower:SetOwner(keys.caster)
 		tower:UpdateHealth(BUILD_TIME,true,.85)
 		tower:SetHullRadius(64)
 		tower:SetControllableByPlayer( keys.caster:GetPlayerID(), true )
