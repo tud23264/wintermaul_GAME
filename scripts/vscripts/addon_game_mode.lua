4c4dccfaeec231ab9a5531cff93d3acb6ecadb77
Eks2 <einceleme@hotmail.com>
2015-08-29 15:28:36 +0300

    Slight change in architecture to match the map creation guidelines specified in https://moddota.com/forums/discussion/135/beginners-guide-to-dota-scripting
    gamemode.lua is now the main lua logic file, where as addon_game_mode.lua is the

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -11,14 +11,9 @@ Wintermaul
 ]]
 
 if CWintermaulGameMode == nil then
-	CWintermaulGameMode = class({})
+	_G.CWintermaulGameMode = class({})		-- I believe the _G prefix specifies a global class
 end
 
-if CustomGameMode == nil then
-	_G.CustomGameMode = class({})
-end
-
-require('gamemode')
 require('utilities')
 require('upgrades')
 require('mechanics')
@@ -33,6 +28,7 @@ require('libraries/notifications')
 
 require("wintermaul_game_round")
 require("wintermaul_game_spawner")
+require('gamemode')
 
 --essential. loads the unit and model needed into memory
 function Precache( context )
@@ -45,206 +41,9 @@ function Precache( context )
 end
 
 function Activate()
-	CustomGameMode:InitGameMode()
 	--activates the mod.
 	GameRules.CWintermaulGameMode = CWintermaulGameMode()
 	--calls InitGameMode
 	GameRules.CWintermaulGameMode:InitGameMode()
 	
 end
-
-
-function CWintermaulGameMode:InitGameMode()
-
-	self._nRoundNumber = 1
-	self._currentRound = nil
-	self._flLastThinkGameTime = nil
-	self._nCurrentSpawnerID = 1
-	print("id: %d", self._nCurrentSpawnerID)
-	self:_ReadGameConfiguration()
-	GameRules:SetTimeOfDay( 0.75 )
-	GameRules:SetHeroRespawnEnabled( false )
-	GameRules:SetUseUniversalShopMode( true )
-	GameRules:SetHeroSelectionTime( 30.0 )
-	GameRules:SetPreGameTime( 10.0 )
-	GameRules:SetPostGameTime( 60.0 )
-	GameRules:SetTreeRegrowTime( 60.0 )
-	--GameRules:SetHeroMinimapIconSize( 600 )
-	GameRules:SetCreepMinimapIconScale( 0.7 )
-	GameRules:SetRuneMinimapIconScale( 0.7 )
-	GameRules:SetGoldTickTime( 60.0 )
-	GameRules:SetGoldPerTick( 0 )
-	GameRules:SetCustomGameTeamMaxPlayers(DOTA_TEAM_GOODGUYS,10)
-	GameRules:SetCustomGameTeamMaxPlayers(DOTA_TEAM_BADGUYS,0)
-	GameRules:GetGameModeEntity():SetRemoveIllusionsOnDeath( true )
-	GameRules:GetGameModeEntity():SetTopBarTeamValuesOverride( true )
-	GameRules:GetGameModeEntity():SetTopBarTeamValuesVisible( false )
-	GameRules:SetSameHeroSelectionEnabled( true )
-	GameRules:GetGameModeEntity():SetFogOfWarDisabled ( true )
-	GameRules:GetGameModeEntity():SetCustomHeroMaxLevel( 1 )
-	GameRules:GetGameModeEntity():SetUseCustomHeroLevels ( true )
-	
-	ListenToGameEvent( "dota_player_pick_hero", Dynamic_Wrap( CWintermaulGameMode, "OnPlayerPicked" ), self )
-	ListenToGameEvent( "game_rules_state_change", Dynamic_Wrap( CWintermaulGameMode, "OnGameRulesStateChange" ), self )
-
-	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 0.25 )
-	
-	print( "Wintermaul is loaded." )
-end
-
--- Read and assign configurable keyvalues if applicable
-function CWintermaulGameMode:_ReadGameConfiguration()
-	local kv = LoadKeyValues( "scripts/maps/wintermaul_map_config.txt" )
-	kv = kv or {} -- Handle the case where there is not keyvalues file
-
-	self._flPrepTimeBetweenRounds = tonumber( kv.PrepTimeBetweenRounds or 0 )
-
-	self:_ReadSpawnsConfiguration( kv["Spawns"] )
-	self:_ReadRoundConfigurations( kv["Waves"])
-end
-
-
--- Verify valid spawns are defined and build a table with them from the keyvalues file
-function CWintermaulGameMode:_ReadSpawnsConfiguration( kvSpawns )
-	self._vSpawnsList = {}
-	if type( kvSpawns ) ~= "table" then
-		return
-	end
-	for _,sp in pairs( kvSpawns ) do			-- Note "_" used as a shortcut to create a temporary throwaway variable
-		table.insert( self._vSpawnsList, {
-			szSpawnerName = sp.SpawnerName or "",
-			szFirstWaypoint = sp.Waypoint or ""
-		} )
-	end
-	for k,v in pairs( self._vSpawnsList ) do
-		print("key: ", k, "val: ", v.szSpawnerName, v.szFirstWaypoint)
-	end
-end
-
--- Set number of rounds without requiring index in text file
-function CWintermaulGameMode:_ReadRoundConfigurations( kv )
-	self._vRounds = {}
-	while true do
-		local szRoundName = string.format("Wave%d", #self._vRounds + 1 )
-		local kvRoundData = kv[ szRoundName ]
-		if kvRoundData == nil then
-			return
-		end
-		local roundObj = CWintermaulGameRound()
-		roundObj:ReadConfiguration( kvRoundData, self, #self._vRounds + 1 )
-		table.insert( self._vRounds, roundObj )
-	end
-end
-
--- When game state changes set state in script
-function CWintermaulGameMode:OnGameRulesStateChange()
-	local nNewState = GameRules:State_Get()
-	if nNewState == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		self._flPrepTimeEnd = GameRules:GetGameTime() + self._flPrepTimeBetweenRounds
-	end
-end
-
--- sets ability points to 0 and sets skills to lvl1 at start.
-function CWintermaulGameMode:OnPlayerPicked( keys )
-	
-	local player = EntIndexToHScript(keys.player)
-	
-	-- Initialize Variables for Tracking
-	player.units = {} -- This keeps the handle of all the units of the player, to iterate for unlocking upgrades
-	player.structures = {} -- This keeps the handle of the constructed units, to iterate for unlocking upgrades
-	player.buildings = {} -- This keeps the name and quantity of each building
-	player.upgrades = {} -- This kees the name of all the upgrades researched
-	player.lumber = 0 -- Secondary resource of the player
-
-
-	for nPlayerID = 0, DOTA_MAX_PLAYERS-1 do
-		if (PlayerResource:IsValidPlayer( nPlayerID ) ) then
-			for e=0,15 do
-				if (PlayerResource:GetPlayer(nPlayerID):GetAssignedHero():GetAbilityByIndex(e) ==nil) then
-					break
-				else
-					PlayerResource:GetPlayer(nPlayerID):GetAssignedHero():GetAbilityByIndex(e):SetLevel(1)
-					PlayerResource:GetPlayer(nPlayerID):GetAssignedHero():SetAbilityPoints(0)
-				end
-			end
-		end
-	end
-end
-
-
-function CWintermaulGameMode:_ThinkPrepTime()
-	if GameRules:GetGameTime() >= self._flPrepTimeEnd then
-		self._flPrepTimeEnd = nil
-		if self._entPrepTimeQuest then
-			UTIL_RemoveImmediate( self._entPrepTimeQuest )
-			self._entPrepTimeQuest = nil
-		end
-
-		if self._nRoundNumber > #self._vRounds then
-			GameRules:SetGameWinner( DOTA_TEAM_GOODGUYS )
-			return false
-		end
-		self._currentRound = self._vRounds[ self._nRoundNumber ]
-		self._currentRound:Begin()
-		return
-	end
-
-	if not self._entPrepTimeQuest then
-		self._entPrepTimeQuest = SpawnEntityFromTableSynchronous( "quest", { name = "PrepTime", title = "#DOTA_Quest_Wintermaul_PrepTime" } )
-		self._entPrepTimeQuest:SetTextReplaceValue( QUEST_TEXT_REPLACE_VALUE_ROUND, self._nRoundNumber )
-
-		self._vRounds[ self._nRoundNumber ]:Precache()
-	end
-	self._entPrepTimeQuest:SetTextReplaceValue( QUEST_TEXT_REPLACE_VALUE_CURRENT_VALUE, self._flPrepTimeEnd - GameRules:GetGameTime() )
-end
-
-
--- this is the thinker. it thinks
--- Evaluate the state of the game
-function CWintermaulGameMode:OnThink()
-	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		--self:_CheckForDefeat()
-		--self:_ThinkLootExpiry()
-
-		if self._flPrepTimeEnd ~= nil then
-			self:_ThinkPrepTime()
-		elseif self._currentRound ~= nil then
-			self._currentRound:Think()
-			if self._currentRound:IsFinished() then
-				self._currentRound:End()
-				self._currentRound = nil
-
-				self._nRoundNumber = self._nRoundNumber + 1
-				if self._nRoundNumber > #self._vRounds then
-					self._nRoundNumber = 1
-					GameRules:MakeTeamLose( DOTA_TEAM_BADGUYS )
-				else
-					self._flPrepTimeEnd = GameRules:GetGameTime() + self._flPrepTimeBetweenRounds
-				end
-			end
-		end
-	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then		-- Safe guard catching any state that may exist beyond DOTA_GAMERULES_STATE_POST_GAME
-		return nil
-	end
-	return 1
-end
-
-function CWintermaulGameMode:OnEntityKilled()
-	--@todo do something when an entity dies
-end
-
---[[
--- Called whenever a player changes its current selection, it keeps a list of entity indexes
-function CWintermaulGameMode:OnPlayerSelectedEntities( event )
-	local pID = event.pID
-
-	GameRules.SELECTED_UNITS[pID] = event.selected_entities
-
-	-- This is for Building Helper to know which is the currently active builder
-	local mainSelected = GetMainSelectedEntity(pID)
-	if IsValidEntity(mainSelected) and IsBuilder(mainSelected) then
-		local player = PlayerResource:GetPlayer(pID)
-		player.activeBuilder = mainSelected
-	end
-end
-]]--
--- scripts/vscripts/gamemode.lua
+++ scripts/vscripts/gamemode.lua
@@ -1,25 +1,55 @@
 
 DEBUG_SPEW = 1
 
-function CustomGameMode:InitGameMode()
+function CWintermaulGameMode:InitGameMode()
 
-	-- Get Rid of Shop button - Change the UI Layout if you want a shop button
+	self._nRoundNumber = 1
+	self._currentRound = nil
+	self._flLastThinkGameTime = nil
+	self._nCurrentSpawnerID = 1
+	print("id: %d", self._nCurrentSpawnerID)
+	self:_ReadGameConfiguration()
+	GameRules:SetTimeOfDay( 0.75 )
+	GameRules:SetHeroRespawnEnabled( false )
+	GameRules:SetUseUniversalShopMode( true )
+	GameRules:SetHeroSelectionTime( 30.0 )
+	GameRules:SetPreGameTime( 10.0 )
+	GameRules:SetPostGameTime( 60.0 )
+	GameRules:SetTreeRegrowTime( 60.0 )
+
+	GameRules:SetCreepMinimapIconScale( 0.7 )
+	GameRules:SetRuneMinimapIconScale( 0.7 )
+	GameRules:SetGoldTickTime( 60.0 )
+	GameRules:SetGoldPerTick( 0 )
+	GameRules:SetCustomGameTeamMaxPlayers(DOTA_TEAM_GOODGUYS,10)
+	GameRules:SetCustomGameTeamMaxPlayers(DOTA_TEAM_BADGUYS,0)
+	GameRules:GetGameModeEntity():SetRemoveIllusionsOnDeath( true )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesOverride( true )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesVisible( false )
+	GameRules:SetSameHeroSelectionEnabled( true )
+	GameRules:GetGameModeEntity():SetFogOfWarDisabled ( true )
+	GameRules:GetGameModeEntity():SetCustomHeroMaxLevel( 1 )
+	GameRules:GetGameModeEntity():SetUseCustomHeroLevels ( true )
+	
+	ListenToGameEvent( "dota_player_pick_hero", Dynamic_Wrap( CWintermaulGameMode, "OnPlayerPicked" ), self )
+	ListenToGameEvent( "game_rules_state_change", Dynamic_Wrap( CWintermaulGameMode, "OnGameRulesStateChange" ), self )
+
+	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 0.25 )
+	
+	-- Gamemode stuff
+	
 	GameRules:GetGameModeEntity():SetHUDVisible(6, false)
 	GameRules:GetGameModeEntity():SetCameraDistanceOverride(1300)
 
 	-- DebugPrint
 	Convars:RegisterConvar('debug_spew', tostring(DEBUG_SPEW), 'Set to 1 to start spewing debug info. Set to 0 to disable.', 1)
-	--[[
-	-- Event Hooks
-	ListenToGameEvent('entity_killed', Dynamic_Wrap(CustomGameMode, 'OnEntityKilled'), self)
-	ListenToGameEvent('dota_player_pick_hero', Dynamic_Wrap(CustomGameMode, 'OnPlayerPickHero'), self)
-	--]]
+
 	-- Filters
-    GameRules:GetGameModeEntity():SetExecuteOrderFilter( Dynamic_Wrap( CustomGameMode, "FilterExecuteOrder" ), self )
+    GameRules:GetGameModeEntity():SetExecuteOrderFilter( Dynamic_Wrap( CWintermaulGameMode, "FilterExecuteOrder" ), self )
 
     -- Register Listener
-    CustomGameEventManager:RegisterListener( "update_selected_entities", Dynamic_Wrap(CustomGameMode, 'OnPlayerSelectedEntities'))
-   	CustomGameEventManager:RegisterListener( "repair_order", Dynamic_Wrap(CustomGameMode, "RepairOrder"))  	
+    CustomGameEventManager:RegisterListener( "update_selected_entities", Dynamic_Wrap(CWintermaulGameMode, 'OnPlayerSelectedEntities'))
+   	CustomGameEventManager:RegisterListener( "repair_order", Dynamic_Wrap(CWintermaulGameMode, "RepairOrder"))  	
     CustomGameEventManager:RegisterListener( "building_helper_build_command", Dynamic_Wrap(BuildingHelper, "BuildCommand"))
 	CustomGameEventManager:RegisterListener( "building_helper_cancel_command", Dynamic_Wrap(BuildingHelper, "CancelCommand"))
 	
@@ -32,18 +62,66 @@ function CustomGameMode:InitGameMode()
 
   	-- Store and update selected units of each pID
 	GameRules.SELECTED_UNITS = {}
-
-	-- Keeps the blighted gridnav positions
-	GameRules.Blight = {}
+	print( "Wintermaul is loaded." )
 end
 
--- A player picked a hero
-function CustomGameMode:OnPlayerPickHero(keys)
+-- Read and assign configurable keyvalues if applicable
+function CWintermaulGameMode:_ReadGameConfiguration()
+	local kv = LoadKeyValues( "scripts/maps/wintermaul_map_config.txt" )
+	kv = kv or {} -- Handle the case where there is not keyvalues file
 
-	local hero = EntIndexToHScript(keys.heroindex)
+	self._flPrepTimeBetweenRounds = tonumber( kv.PrepTimeBetweenRounds or 0 )
+
+	self:_ReadSpawnsConfiguration( kv["Spawns"] )
+	self:_ReadRoundConfigurations( kv["Waves"])
+end
+
+
+-- Verify valid spawns are defined and build a table with them from the keyvalues file
+function CWintermaulGameMode:_ReadSpawnsConfiguration( kvSpawns )
+	self._vSpawnsList = {}
+	if type( kvSpawns ) ~= "table" then
+		return
+	end
+	for _,sp in pairs( kvSpawns ) do			-- Note "_" used as a shortcut to create a temporary throwaway variable
+		table.insert( self._vSpawnsList, {
+			szSpawnerName = sp.SpawnerName or "",
+			szFirstWaypoint = sp.Waypoint or ""
+		} )
+	end
+	for k,v in pairs( self._vSpawnsList ) do
+		print("key: ", k, "val: ", v.szSpawnerName, v.szFirstWaypoint)
+	end
+end
+
+-- Set number of rounds without requiring index in text file
+function CWintermaulGameMode:_ReadRoundConfigurations( kv )
+	self._vRounds = {}
+	while true do
+		local szRoundName = string.format("Wave%d", #self._vRounds + 1 )
+		local kvRoundData = kv[ szRoundName ]
+		if kvRoundData == nil then
+			return
+		end
+		local roundObj = CWintermaulGameRound()
+		roundObj:ReadConfiguration( kvRoundData, self, #self._vRounds + 1 )
+		table.insert( self._vRounds, roundObj )
+	end
+end
+
+-- When game state changes set state in script
+function CWintermaulGameMode:OnGameRulesStateChange()
+	local nNewState = GameRules:State_Get()
+	if nNewState == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		self._flPrepTimeEnd = GameRules:GetGameTime() + self._flPrepTimeBetweenRounds
+	end
+end
+
+-- sets ability points to 0 and sets skills to lvl1 at start.
+function CWintermaulGameMode:OnPlayerPicked( keys )
+	
 	local player = EntIndexToHScript(keys.player)
-	local playerID = hero:GetPlayerID()
-
+	
 	-- Initialize Variables for Tracking
 	player.units = {} -- This keeps the handle of all the units of the player, to iterate for unlocking upgrades
 	player.structures = {} -- This keeps the handle of the constructed units, to iterate for unlocking upgrades
@@ -51,134 +129,85 @@ function CustomGameMode:OnPlayerPickHero(keys)
 	player.upgrades = {} -- This kees the name of all the upgrades researched
 	player.lumber = 0 -- Secondary resource of the player
 
-    -- Create city center in front of the hero
-    local position = hero:GetAbsOrigin() + hero:GetForwardVector() * 300
-    local city_center_name = "city_center"
-	local building = BuildingHelper:PlaceBuilding(player, city_center_name, position, true, 5) 
 
-	-- Set health to test repair
-	building:SetHealth(building:GetMaxHealth()/3)
-
-	-- These are required for repair to know how many resources the building takes
-	building.GoldCost = 100
-	building.LumberCost = 100
-	building.BuildTime = 15
-
-	-- Add the building to the player structures list
-	player.buildings[city_center_name] = 1
-	table.insert(player.structures, building)
-
-	CheckAbilityRequirements( hero, player )
-	CheckAbilityRequirements( building, player )
-
-	-- Add the hero to the player units list
-	table.insert(player.units, hero)
-	hero.state = "idle" --Builder state
-
-	-- Spawn some peasants around the hero
-	local position = hero:GetAbsOrigin()
-	local numBuilders = 5
-	local angle = 360/numBuilders
-	for i=1,5 do
-		local rotate_pos = position + Vector(1,0,0) * 100
-		local builder_pos = RotatePosition(position, QAngle(0, angle*i, 0), rotate_pos)
-
-		local builder = CreateUnitByName("peasant", builder_pos, true, hero, hero, hero:GetTeamNumber())
-		builder:SetOwner(hero)
-		builder:SetControllableByPlayer(playerID, true)
-		table.insert(player.units, builder)
-		builder.state = "idle"
-
-		-- Go through the abilities and upgrade
-		CheckAbilityRequirements( builder, player )
+	for nPlayerID = 0, DOTA_MAX_PLAYERS-1 do
+		if (PlayerResource:IsValidPlayer( nPlayerID ) ) then
+			for e=0,15 do
+				if (PlayerResource:GetPlayer(nPlayerID):GetAssignedHero():GetAbilityByIndex(e) ==nil) then
+					break
+				else
+					PlayerResource:GetPlayer(nPlayerID):GetAssignedHero():GetAbilityByIndex(e):SetLevel(1)
+					PlayerResource:GetPlayer(nPlayerID):GetAssignedHero():SetAbilityPoints(0)
+				end
+			end
+		end
 	end
-
-	-- Give Initial Resources
-	hero:SetGold(5000, false)
-	ModifyLumber(player, 5000)
-
-	-- Lumber tick
-	Timers:CreateTimer(1, function()
-		ModifyLumber(player, 10)
-		return 10
-	end)
-
-	-- Give a building ability
-	local item = CreateItem("item_build_wall", hero, hero)
-	hero:AddItem(item)
-
-	-- Learn all abilities (this isn't necessary on creatures)
-	for i=0,15 do
-		local ability = hero:GetAbilityByIndex(i)
-		if ability then ability:SetLevel(ability:GetMaxLevel()) end
-	end
-	hero:SetAbilityPoints(0)
-
 end
 
--- An entity died
-function CustomGameMode:OnEntityKilled( event )
 
-	-- The Unit that was Killed
-	local killedUnit = EntIndexToHScript(event.entindex_killed)
-	-- The Killing entity
-	local killerEntity
-	if event.entindex_attacker then
-		killerEntity = EntIndexToHScript(event.entindex_attacker)
+function CWintermaulGameMode:_ThinkPrepTime()
+	if GameRules:GetGameTime() >= self._flPrepTimeEnd then
+		self._flPrepTimeEnd = nil
+		if self._entPrepTimeQuest then
+			UTIL_RemoveImmediate( self._entPrepTimeQuest )
+			self._entPrepTimeQuest = nil
+		end
+
+		if self._nRoundNumber > #self._vRounds then
+			GameRules:SetGameWinner( DOTA_TEAM_GOODGUYS )
+			return false
+		end
+		self._currentRound = self._vRounds[ self._nRoundNumber ]
+		self._currentRound:Begin()
+		return
 	end
 
-	-- Player owner of the unit
-	local player = killedUnit:GetPlayerOwner()
+	if not self._entPrepTimeQuest then
+		self._entPrepTimeQuest = SpawnEntityFromTableSynchronous( "quest", { name = "PrepTime", title = "#DOTA_Quest_Wintermaul_PrepTime" } )
+		self._entPrepTimeQuest:SetTextReplaceValue( QUEST_TEXT_REPLACE_VALUE_ROUND, self._nRoundNumber )
 
-	-- Building Killed
-	if IsCustomBuilding(killedUnit) then
-
-		 -- Building Helper grid cleanup
-		BuildingHelper:RemoveBuilding(killedUnit, true)
-
-		-- Check units for downgrades
-		local building_name = killedUnit:GetUnitName()
-				
-		-- Substract 1 to the player building tracking table for that name
-		if player.buildings[building_name] then
-			player.buildings[building_name] = player.buildings[building_name] - 1
-		end
-
-		-- possible unit downgrades
-		for k,units in pairs(player.units) do
-		    CheckAbilityRequirements( units, player )
-		end
-
-		-- possible structure downgrades
-		for k,structure in pairs(player.structures) do
-			CheckAbilityRequirements( structure, player )
-		end
+		self._vRounds[ self._nRoundNumber ]:Precache()
 	end
+	self._entPrepTimeQuest:SetTextReplaceValue( QUEST_TEXT_REPLACE_VALUE_CURRENT_VALUE, self._flPrepTimeEnd - GameRules:GetGameTime() )
+end
 
-	-- Table cleanup
-	if player then
-		-- Remake the tables
-		local table_structures = {}
-		for _,building in pairs(player.structures) do
-			if building and IsValidEntity(building) and building:IsAlive() then
-				--print("Valid building: "..building:GetUnitName())
-				table.insert(table_structures, building)
+
+-- this is the thinker. it thinks
+-- Evaluate the state of the game
+function CWintermaulGameMode:OnThink()
+	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		--self:_CheckForDefeat()
+		--self:_ThinkLootExpiry()
+
+		if self._flPrepTimeEnd ~= nil then
+			self:_ThinkPrepTime()
+		elseif self._currentRound ~= nil then
+			self._currentRound:Think()
+			if self._currentRound:IsFinished() then
+				self._currentRound:End()
+				self._currentRound = nil
+
+				self._nRoundNumber = self._nRoundNumber + 1
+				if self._nRoundNumber > #self._vRounds then
+					self._nRoundNumber = 1
+					GameRules:MakeTeamLose( DOTA_TEAM_BADGUYS )
+				else
+					self._flPrepTimeEnd = GameRules:GetGameTime() + self._flPrepTimeBetweenRounds
+				end
 			end
 		end
-		player.structures = table_structures
-		
-		local table_units = {}
-		for _,unit in pairs(player.units) do
-			if unit and IsValidEntity(unit) then
-				table.insert(table_units, unit)
-			end
-		end
-		player.units = table_units		
+	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then		-- Safe guard catching any state that may exist beyond DOTA_GAMERULES_STATE_POST_GAME
+		return nil
 	end
+	return 1
+end
+
+function CWintermaulGameMode:OnEntityKilled()
+	--@todo do something when an entity dies
 end
 
 -- Called whenever a player changes its current selection, it keeps a list of entity indexes
-function CustomGameMode:OnPlayerSelectedEntities( event )
+function CWintermaulGameMode:OnPlayerSelectedEntities( event )
 	local pID = event.pID
 
 	GameRules.SELECTED_UNITS[pID] = event.selected_entities
